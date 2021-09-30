b37d64efa260437d525b0b3bac53b9352c1a159b
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-09-30 18:00:20 +0300

    5 test (wrong)

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", () => {
-  if (searchInput.value == "google") {
-    alert("Yandex круче. Но это не точно");
+  if (searchInput.value == `google + ${objArr[0]}`) {
+    alert(`Yandex круче. Но это не точно`);
   } else alert(searchInput.value);
 });
+
+let objArr = [
+  { name: "Ivan", age: "23" },
+  { name: "Alex", age: "24" },
+  { name: "Vasya", age: "22" },
+];

8d344ada9c3de5c65c08143731cb19110215c8c4
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-09-30 17:51:52 +0300

    test 4 case

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", () => {
-  alert(searchInput.value);
+  if (searchInput.value == "google") {
+    alert("Yandex круче. Но это не точно");
+  } else alert(searchInput.value);
 });
